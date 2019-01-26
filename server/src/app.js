74afb712b9602473fe6155294a891900309616c5
Kendall Adkins <adkins.kendall90@gmail.com>
2019-01-26 16:13:30 -0500

    server directory created

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+
+console.log('server started');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const port = 8081;
+
+const app = express()
+  .use(morgan('combined'))
+  .use(bodyParser.json())
+  .use(cors());
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello there from DOTA 2 drafter! server is online!',
+  });
+});
+
+app.listen(process.env.PORT || port);
